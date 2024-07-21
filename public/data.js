9d744c305354c3d5a550521af1a7686edb765b03
Piecharm <deeprockz93@gmail.com>
2024-07-21 14:59:50 +0530

    added styles images & data

---
+++ public/data.js
@@ -0,0 +1,38 @@
+export const riceData = [
+    {
+        name: "Kadhi Rice",
+        price: 15,
+        photoName: "rices/Kadhi Rice.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Khichdi",
+        price: 10,
+        photoName: "rices/Khichdi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Lemon Rice",
+        price: 12,
+        photoName: "rices/Lemon Rice.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pulao",
+        price: 18,
+        photoName: "rices/Pulao.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Rajma Chawal",
+        price: 18,
+        photoName: "rices/Rajma Chawal.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Tomato Rice",
+        price: 18,
+        photoName: "rices/Tomato Rice.jpg",
+        soldOut: false,
+    },
+];
